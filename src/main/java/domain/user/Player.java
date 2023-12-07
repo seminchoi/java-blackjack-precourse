81f1d72b90539dfbf88764e9e0384751f0dd2c15
seminchoi <choicco89@gmail.com>
2023-12-07 16:31:50 +0900

    feat(domain.user.Player) : 베팅머니 유효성 검증 추가

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -10,6 +10,8 @@ import java.util.List;
  * 게임 참여자를 의미하는 객체
  */
 public class Player {
+    private final static double MIN_BETTING_MONEY = 10;
+
     private final String name;
     private final double bettingMoney;
     private final BlackjackCards cards = new BlackjackCards();
@@ -17,6 +19,14 @@ public class Player {
     public Player(String name, double bettingMoney) {
         this.name = name;
         this.bettingMoney = bettingMoney;
+    }
+
+    private void validate(final double bettingMoney) {
+        if(bettingMoney < MIN_BETTING_MONEY) {
+            throw new IllegalArgumentException(
+                    String.format("배팅 머니는 %f 이상이어야 합니다.", MIN_BETTING_MONEY)
+            );
+        }
     }
 
     public void addCard(Card card) {
