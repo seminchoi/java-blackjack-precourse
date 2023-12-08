8cb68284a5d8631e220246e7b0501395484fdc5f
seminchoi <choicco89@gmail.com>
2023-12-08 11:45:18 +0900

    style(Player) : TODO 주석 작성

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -9,6 +9,9 @@ import java.util.List;
 /**
  * 게임 참여자를 의미하는 객체
  */
+// 헌치님 코드 참고
+// TODO : 참여자라는 abstract 클래스를 만들어서 추상화
+// TODO : STATE 패턴을 통해 현재 진행중인지, 아직 끝나지 않았는지 확인
 public class Player {
     private final static double MIN_BETTING_MONEY = 10;
 
@@ -17,6 +20,7 @@ public class Player {
     private final BlackjackCards cards = new BlackjackCards();
 
     public Player(String name, double bettingMoney) {
+        validate(bettingMoney);
         this.name = name;
         this.bettingMoney = bettingMoney;
     }
@@ -43,10 +47,6 @@ public class Player {
 
     public int calculateScore() {
         return cards.calculateScore();
-    }
-
-    public Card getFirstCard() {
-        return cards.getFirstCard();
     }
 
     public List<Card> getCards() {
