72d974f11d3182d8737781ee09f15c9810602b42
seminchoi <choicco89@gmail.com>
2023-12-09 21:20:58 +0900

    refactor(StayState) : 이율 계산 메소드 구현

--- src/main/java/domain/user/state/StayState.java
+++ src/main/java/domain/user/state/StayState.java
@@ -1,6 +1,22 @@
 package domain.user.state;
 
+import domain.blackjack.BlackjackCards;
+
 public class StayState implements State {
+    @Override
+    public double getProfitRate(final BlackjackCards playerHands, final BlackjackCards dealerHands) {
+        if (!dealerHands.isBust() && playerHands.calculateScore() < dealerHands.calculateScore()) {
+            return LOSE_PROFIT;
+        }
+        if (dealerHands.isBust() || playerHands.calculateScore() > dealerHands.calculateScore()) {
+            return WIN_PROFIT;
+        }
+        if(playerHands.calculateScore() == dealerHands.calculateScore()) {
+            return DRAW_PROFIT;
+        }
+        throw new InternalError("이익율 계산에 실패했습니다.");
+    }
+
     @Override
     public boolean isFinished() {
         return true;
