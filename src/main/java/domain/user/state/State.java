73f8390a2e170d6ee758322686db4e2dcfeb9f19
seminchoi <choicco89@gmail.com>
2023-12-09 20:22:59 +0900

    refactor(State) : addCard() 메소드의 매개변수 변경

--- src/main/java/domain/user/state/State.java
+++ src/main/java/domain/user/state/State.java
@@ -2,13 +2,14 @@ package domain.user.state;
 
 import domain.blackjack.BlackjackCards;
 import domain.card.Card;
+import domain.card.CardDeck;
 
 public interface State {
     default double getProfitRate() {
         throw new UnsupportedOperationException("현재 상태에서는 이익율을 계산할 수 없습니다.");
     }
 
-    default void addCard(final BlackjackCards blackjackCards, final Card card) {
+    default void addCard(final CardDeck cardDeck, final BlackjackCards blackjackCards) {
         throw new UnsupportedOperationException("현재 상태에서는 카드를 추가할 수 없습니다.");
     }
 
